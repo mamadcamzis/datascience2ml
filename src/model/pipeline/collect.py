4ef80c2508216b11ca3f2e34274727a595086226
Mamadou Camara <camara0388@gmail.com>
2024-09-20 17:59:34 +0200

    modelisation

---
+++ src/model/pipeline/collect.py
@@ -0,0 +1,50 @@
+import os
+import pandas as pd
+from pydantic import FilePath
+from config.config import settings, engine
+from databases.db_model import RentApartments
+from sqlalchemy import select
+from loguru import logger
+
+
+def load_data(path: FilePath = settings.DATA_FILE_NAME) -> pd.DataFrame:
+    """
+    Charge un fichier CSV à partir d'un chemin donné et le renvoie sous forme de DataFrame pandas.
+
+    Paramètres:
+    -----------
+    path : FilePath, optionnel
+        Le chemin vers le fichier CSV à charger. Par défaut, il utilise la variable `settings.DATA_FILE_NAME`.
+
+    Retour:
+    -------
+    pandas.DataFrame
+        Un DataFrame contenant les données du fichier CSV.
+
+    Exemple:
+    --------
+    >>> df = load_data('data/raw/my_data.csv')
+    >>> print(df.head())
+    """
+    csv_path = os.path.join(settings.DATA_PATH, path)
+    logger.info(f"Loading csv file at {csv_path} ...")
+    return pd.read_csv(csv_path)
+
+
+def load_data_from_db() -> pd.DataFrame:
+    """
+    Charge les données à partir d'une base de données SQLite.
+
+    Retour:
+    -------
+    pandas.DataFrame
+        Un DataFrame contenant les données de la table 'data'.
+
+    Exemple:
+    --------
+    >>> df = load_data_from_db()
+    >>> print(df.head())
+    """
+    logger.info("Loading data from database ...")
+    query = select(RentApartments)
+    return pd.read_sql(query, engine)
---
+++ src/model/pipeline/model.py
@@ -0,0 +1,212 @@
+
+import os
+import joblib
+import pandas as pd 
+from typing import List, Tuple
+from sklearn.base import BaseEstimator
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split, GridSearchCV
+from loguru import logger
+from model.pipeline.preparation import prepare_data
+from config.config import settings
+
+
+def build_model() -> None:
+    """
+    Construit, entraîne, évalue et sauvegarde un modèle de classification.
+
+    Notes
+    -----
+    Cette fonction utilise les fonctions suivantes définies ailleurs :
+    - prepare_data() : Prépare les données pour l'entraînement du modèle.
+    - get_X_y(data) : Extrait les caractéristiques et la variable cible à partir des données.
+    - split_train_test(X, y) : Divise les données en ensembles d'entraînement et de test.
+    - train_model(X_train, y_train) : Entraîne le modèle avec les données d'entraînement.
+    - evaluate_model(model, X_test, y_test) : Évalue les performances du modèle sur l'ensemble de test.
+    - save_model(model) : Sauvegarde le modèle entraîné.
+
+    """
+    logger.info("Starting  Building Model Pipeline ...")
+    # Préparation des données
+    data = prepare_data()
+
+    # Extraction des caractéristiques et de la variable cible
+    X, y = get_X_y(data)
+
+    # Division en ensembles d'entraînement et de test
+    X_train, X_test, y_train, y_test = split_train_test(X, y)
+
+    # Entraînement du modèle
+    rf_classifier = train_model(X_train, y_train)
+
+    # Évaluation du modèle
+    evaluate_model(rf_classifier, X_test, y_test)
+
+    # Sauvegarde du modèle entraîné
+    save_model(rf_classifier)
+
+    # Affichage du score obtenu
+    #print(f"Score is: {score:.2f}")
+
+
+
+
+def get_X_y(
+    data: pd.DataFrame, 
+    col_x: List[str] = ['area', 'constraction_year', 'bedrooms', 'garden', 
+                        'balcony_yes', 'parking_yes', 'furnished_yes', 
+                        'garage_yes', 'storage_yes'], 
+    col_y: str = 'rent'
+) -> Tuple[pd.DataFrame, pd.Series]:
+    """
+    Extrait les caractéristiques (X) et la variable cible (y) d'un DataFrame.
+
+    Parameters
+    ----------
+    data : pandas.DataFrame
+        Le DataFrame contenant les données.
+    col_x : List[str], optional
+        La liste des colonnes à utiliser comme caractéristiques. 
+        Les colonnes par défaut sont ['area', 'constraction_year', 
+        'bedrooms', 'garden', 'balcony_yes', 'parking_yes', 
+        'furnished_yes', 'garage_yes', 'storage_yes'].
+    col_y : str, optional
+        Le nom de la colonne à utiliser comme variable cible. 
+        La colonne par défaut est 'rent'.
+
+    Returns
+    -------
+    Tuple[pd.DataFrame, pd.Series]
+        Un tuple contenant deux éléments :
+        - X : pandas.DataFrame
+            Le DataFrame des caractéristiques.
+        - y : pandas.Series
+            La série représentant la variable cible.
+    """
+    logger.info("Getting X, y data ...")
+    X = data[col_x]
+    y = data[col_y]
+    return X, y
+    
+
+def split_train_test(
+    X: pd.DataFrame, 
+    y: pd.Series, 
+    test_size: float = 0.2
+) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
+    """
+    Sépare les données en ensembles d'entraînement et de test.
+
+    Parameters
+    ----------
+    X : pandas.DataFrame
+        Le DataFrame contenant les caractéristiques.
+    y : pandas.Series
+        La série représentant la variable cible.
+    test_size : float, optional
+        La proportion de l'ensemble de données à inclure dans l'ensemble de test. 
+        La valeur par défaut est 0.2.
+
+    Returns
+    -------
+    Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]
+        Un tuple contenant quatre éléments :
+        - X_train : pandas.DataFrame
+            Le DataFrame des caractéristiques pour l'ensemble d'entraînement.
+        - X_test : pandas.DataFrame
+            Le DataFrame des caractéristiques pour l'ensemble de test.
+        - y_train : pandas.Series
+            La série représentant la variable cible pour l'ensemble d'entraînement.
+        - y_test : pandas.Series
+            La série représentant la variable cible pour l'ensemble de test.
+    """
+    logger.info("Splitting data in train and test")
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
+    return X_train, X_test, y_train, y_test
+
+
+
+
+def train_model(
+    x_train: pd.DataFrame, 
+    y_train: pd.Series
+) -> BaseEstimator:
+    """
+    Entraîne un modèle de classification avec les données fournies.
+
+    Parameters
+    ----------
+    x_train : pandas.DataFrame
+        Le DataFrame contenant les caractéristiques.
+    y_train : pandas.Series
+        La série représentant la variable cible.
+
+
+    Returns
+    -------
+    BaseEstimator
+        Le modèle de classification entraîné.
+    """
+    logger.info("Training model and tunning hyperparams ...")
+    rf_classifier = RandomForestRegressor()
+    grid_space = {'n_estimators': [100, 200, 300], 'max_depth': [3, 6, 9, 12]}
+    logger.debug(f"Grid Space is {grid_space}  ..")
+    grid = GridSearchCV(rf_classifier, param_grid=grid_space, cv=5, scoring = 'r2')
+    model_grid = grid.fit(x_train, y_train)
+    
+    return  model_grid.best_estimator_
+
+    
+def evaluate_model(
+    model: BaseEstimator, 
+    X_test: pd.DataFrame, 
+    y_test: pd.Series
+) -> float:
+    """
+    Évalue les performances d'un modèle sur un ensemble de test.
+
+    Parameters
+    ----------
+    model : BaseEstimator
+        Le modèle de classification entraîné.
+    X_test : pandas.DataFrame
+        Le DataFrame des caractéristiques pour l'ensemble de test.
+    y_test : pandas.Series
+        La série représentant la variable cible pour l'ensemble de test.
+
+    Returns
+    -------
+    float
+        Le score de performance du modèle sur l'ensemble de test.
+    """
+    logger.info(f"Evaluating Model, Score is {model.score(X_test, y_test):.2f}")
+    score = model.score(X_test, y_test)
+    return score
+
+
+def save_model(model):
+    """
+    Sauvegarde le modèle à la fois en format pickle et joblib dans le répertoire MODELS_DIR.
+
+    Parameters
+    ----------
+    model : object
+        Le modèle à sauvegarder. Il doit être sérialisable.
+
+    Notes
+    -----
+    Cette fonction utilise les variables globales model_name et version pour générer
+    les noms de fichiers. Assurez-vous que ces variables sont définies correctement
+    avant d'appeler cette fonction.
+    """
+    
+  
+    joblib_model = settings.MODELS_NAME + '_v_' + settings.VERSION + '.joblib'
+    
+    persist_path = os.path.join(settings.MODELS_PATH, joblib_model)  
+    logger.info(f"Saving Model at {persist_path}")
+    # Sauvegarde en format joblib
+    joblib.dump(model, persist_path)
+
+
+#build_model()
