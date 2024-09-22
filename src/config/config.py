e0da08d3a430ec7800d95d6307bb9e421f863267
Mamadou Camara <camara0388@gmail.com>
2024-09-22 14:02:00 +0200

    Update code

--- src/config/config.py
+++ src/config/config.py
@@ -1,6 +1,33 @@
+"""
+This module sets up the configuration for the data science project using Pydantic for settings management,
+Loguru for logging, and SQLAlchemy for database connection.
+Attributes:
+    CONFI_DIR (Path): The directory where the configuration file is located.
+    PARENT_DIR (Path): The parent directory of the configuration directory.
+    LOGER_DIR (Path): The directory where log files will be stored.
+    ENV_FILE (Path): The path to the environment file.
+    LOGGER_FILE (Path): The path to the log file.
+Classes:
+    Settings(BaseSettings): A Pydantic model that loads settings from the environment file.
+        Attributes:
+            model_config (SettingsConfigDict): Configuration for loading environment variables.
+            DATA_FILE_NAME (str): The name of the data file.
+            DATA_PATH (DirectoryPath): The directory path where data files are stored.
+            MODELS_PATH (DirectoryPath): The directory path where model files are stored.
+            MODELS_NAME (str): The name of the model.
+            VERSION (str): The version of the project.
+            LOG_LEVEL (str): The logging level.
+            DB_CONNECTION (str): The database connection string.
+            TABLE_NAME (str): The name of the database table.
+Variables:
+    settings (Settings): An instance of the Settings class with loaded configuration.
+    engine (Engine): An SQLAlchemy engine instance for database connection.
+"""
+
 from pydantic import DirectoryPath
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pathlib import Path
+
 from loguru import logger
 from sqlalchemy import create_engine
 
@@ -11,24 +38,39 @@ LOGER_DIR = PARENT_DIR / 'logs'
 ENV_FILE = CONFI_DIR / '.env'
 LOGGER_FILE = LOGER_DIR / 'app.log'
 
-
 logger.info(f"Setting Config: {ENV_FILE}")
 
 class Settings(BaseSettings):
+    """A Pydantic model that loads settings from the environment file."""
+    
     model_config = SettingsConfigDict(env_file=ENV_FILE, env_file_encoding='utf-8')
-    DATA_FILE_NAME : str
+    DATA_FILE_NAME: str
     DATA_PATH: DirectoryPath
-    MODELS_PATH : DirectoryPath
-    MODELS_NAME : str 
-    VERSION : str 
-    LOG_LEVEL : str
-    DB_CONNECTION : str
-    TABLE_NAME : str
+    MODELS_PATH: DirectoryPath
+    MODELS_NAME: str
+    VERSION: str
+    LOG_LEVEL: str
+    DB_CONNECTION: str
+    TABLE_NAME: str
   
     
-settings = Settings()
+def configure_logging(log_level: str) -> None:
+    """This function removes any existing loggers and adds a new logger with the specified settings.
+    The log files are rotated daily, retained for two days, and compressed in zip format.
+    Args:
+        log_level (str): The logging level to be set for the logger.
+    Returns:
+        None
+    """
+    
+    logger.remove()
+    logger.add(LOGGER_FILE, 
+               rotation="1 day",
+               retention='2 days',
+               compression="zip",
+               level=log_level)
 
-logger.remove()
-logger.add(LOGGER_FILE, rotation="1 day", compression="zip", level=settings.LOG_LEVEL)
+settings = Settings()
+configure_logging(settings.LOG_LEVEL)
 #engine = create_engine(f"sqlite:///{DB_DIR}/{settings.db_connection}") #echo=True
 engine = create_engine(settings.DB_CONNECTION)
--- src/model/model_service.py
+++ src/model/model_service.py
@@ -1,22 +1,40 @@
 import joblib
+from pathlib import Path
 
 import pandas as pd
-
-from pathlib import Path
+from loguru import logger
 from config.config import settings
 from model.pipeline.model import build_model
 
-from loguru import logger
-
 
 class ModelService:
+    """A service class for handling machine learning model operations such as loading a model and making predictions.
+    Attributes:
+        model (Any): The machine learning model instance.
+        model_name (str): The name of the model to be loaded.
+    Methods:
+        __init__(): Initializes the ModelService with default values.
+        load_model(model_name=None): Loads the machine learning model from a file.
+        predict(input_parameters): Predicts the output using the loaded model based on input parameters.
+        Initializes the ModelService instance with default values.
+        Loads the machine learning model from a file. If the model file does not exist, it triggers the model building process.
+        Args:
+            model_name (str, optional): The name of the model to be loaded. Defaults to None.
+        Predicts the output using the loaded model based on input parameters.
+        Args:
+            input_parameters (dict or pd.DataFrame): The input parameters for making predictions. If a dictionary is provided, it will be converted to a DataFrame.
+        Returns:
+            np.ndarray: The predicted values.
+    """
     
     def __init__(self) -> None:
+        """Initializes the ModelService with default values."""
         
         self.model = None
         self.model_name = settings.MODELS_NAME
     
-    def load_model(self, model_name=None):
+    def load_model(self, model_name=None) -> None:
+        """Loads the machine learning model from a file."""
         
         logger.info("Checking the existence of model config file ...")
         if model_name:
@@ -29,23 +47,28 @@ class ModelService:
             logger.warning(f"Model not found at {model_path} -> building a new {settings.MODELS_NAME} model ...")
             build_model()
         
-        
         logger.info(f"Model {settings.MODELS_NAME} exists -> Loading Model from {model_path} ...")   
-        self.model = joblib.load(model_path)
+        with open(model_path, 'rb') as file:
+            self.model = joblib.load(file)
         
-    def predict(self, input_parameters):
+    def predict(self, input_parameters: list) -> list:
+        """Predicts the output using the loaded model based on input parameters.
+        Parameters:
+        -----------
+        input_parameters: list or pd.DataFrame
+          
+        Returns:
+        --------
+        list:
+            The predicted output from the model.
+        
+        """
         
         logger.info(f"Predicting the price of the house with the following parameters {input_parameters} ...")
         if not isinstance(input_parameters, pd.DataFrame):
             input_parameters = pd.DataFrame([input_parameters], 
-                                    columns=['area',  'constraction_year',  'bedrooms', 
+                                            columns=['area',  'constraction_year', 'bedrooms', 
                                             'garden', 'balcony_yes', 'parking_yes', 
                                             'furnished_yes', 'garage_yes', 'storage_yes'])
             
         return self.model.predict(input_parameters)
-    
-    
-    
-
-
-            
