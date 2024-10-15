fa21a6ecca60237f7c243104b2209f7851792aea
Mamadou Camara <camara0388@gmail.com>
2024-10-15 03:34:37 +0200

    init package

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+"""
+This is the initialization module for the datascience2ml package.
+
+It is used to initialize the package and can include import statements
+and package-level variables or functions.
+"""
